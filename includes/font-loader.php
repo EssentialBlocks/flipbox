8fead43e7abe72ab1e20403d7ccc418f27142d5f
Tasnim Alam <tasnimalamcse@gmail.com>
2020-07-26 13:06:04 +0600

    Change font loader prefix EB => Flipbox

--- includes/font-loader.php
+++ includes/font-loader.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class EB_Font_Loader
+class Flipbox_Font_Loader
 {
 
     private static $instance;
@@ -91,4 +91,4 @@ class EB_Font_Loader
         }
     }
 }
-EB_Font_Loader::register();
+Flipbox_Font_Loader::register();
