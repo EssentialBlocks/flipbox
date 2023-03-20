59e3d8f4bbd6e045b4c17f8f4b8133aed07fe00f
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2023-03-20 15:18:46 +0600

    update controls

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8ddec0ce1b9bb92b470f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '577bdcebe803116feab7');
