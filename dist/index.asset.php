0746a44bc83858a4c25d1b51a6e8ffd610946f92
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2023-03-20 15:19:19 +0600

    update controls

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7f086c0ab58d628f6f5b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5bda652b594cc1c809c9');
